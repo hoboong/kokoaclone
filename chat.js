2d652575628de2f5ca0f22069247356066647118
joon4366 <rlghwns1125@gmail.com>
2020-09-25 16:06:20 +0900

    at work

---
+++ chat.js
@@ -0,0 +1,20 @@
+MAIN_CHAT = document.querySelector(".main-chat");
+
+
+
+function saveMessage(text) {
+    
+}
+
+function handleTyping()
+
+function paintMessage(text){
+    
+}
+
+
+function init() {
+
+}
+
+init();
