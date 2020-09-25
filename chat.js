54da104056e2ee94d33cf6bbd8f7b40a68eb984c
joon4366 <rlghwns1125@gmail.com>
2020-09-25 17:32:58 +0900

    9/25

--- chat.js
+++ chat.js
@@ -1,20 +1,34 @@
-MAIN_CHAT = document.querySelector(".main-chat");
+const MAIN_CHAT = document.querySelector(".main-chat"),
+    INPUT = document.querySelector("input");
 
+const MESSAGES_LS = "messages"
 
+function loadMessages(){
+    const message = localStorage.getItem(MESSAGES_LS);
+    if(message !== null) {
+
+    }
+}
 
 function saveMessage(text) {
     
 }
 
-function handleTyping()
+function handleSubmit() {
+    event.preventDefault();
+    const currentValue = INPUT.value;
+    paintMessage(currentValue);
+}
 
 function paintMessage(text){
-    
+    console.log(text);
 }
 
 
 function init() {
-
+    loadMessages();
+    MAIN_CHAT.addEventListener("submit", handleSubmit);
 }
+
 
 init();
